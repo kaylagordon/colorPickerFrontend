6d4f6bed96d7779be3255a0b31fbad7e4aa3d572
Ben Firth <bfirth93@gmail.com>
2020-02-06 15:24:20 -0700

    Add projectReducer code

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -1,7 +1,9 @@
-export const addedProject = (state = {}, action) => {
+export const Project = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_PROJECT':
-      return action.project;
+      return [...state, action.project];
+    case 'DELETE_PROJECT':
+      return [...state].filter(project => project.id !== action.id);
     default:
       return state;
   }
