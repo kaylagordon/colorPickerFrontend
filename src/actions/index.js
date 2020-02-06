389d2d8796fa5f27e6f4d8b934fecc6bd5cf2b24
Ben Firth <bfirth93@gmail.com>
2020-02-06 14:51:56 -0700

    Add actions

---
+++ src/actions/index.js
@@ -0,0 +1,31 @@
+export const addPalette = (id, color1, color2, color3, color4, color5, name, project_id) => ({
+  type: 'ADD_PALETTE',
+  palette: {
+    id,
+    color1,
+    color2,
+    color3,
+    color4,
+    color5,
+    name,
+    project_id
+  }
+});
+
+export const addProject = (name, id) => ({
+  type: 'ADD_PROJECT',
+  project: {
+    name,
+    id
+  }
+});
+
+export const deletePalette = id => ({
+  type: 'DELETE_PALETTE',
+  id
+});
+
+export const deleteProject = id => ({
+  type: 'DELETE_PALETTE',
+  id
+});
