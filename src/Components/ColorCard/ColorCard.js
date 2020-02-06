b8cb5cbfcdc98f10fef60b4e728faee2257590e2
Kayla Wood <kaylaewood@gmail.com>
2020-02-06 15:06:41 -0700

    Make hex codes uppercase on page

--- src/Components/ColorCard/ColorCard.js
+++ src/Components/ColorCard/ColorCard.js
@@ -4,16 +4,15 @@ import PropTypes from 'prop-types';
 
 function ColorCard() {
   let randomColor = Math.floor(Math.random()*16777215).toString(16);
-//set global state to randomColor
+  //set global state to randomColor
   const [ locked, switchLock ] = useState();
-
 
   return (
     <div className='color-card' style={{
       backgroundColor: `#${randomColor}`
     }}>
       <p>lock</p>
-      <p>######</p>
+      <p>#{randomColor.toUpperCase()}</p>
     </div>
   );
 }
