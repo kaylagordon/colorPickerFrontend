7b077bd62e4c98a2209643b6c9c65ec21bde54e1
Ben Firth <bfirth93@gmail.com>
2020-02-06 15:28:12 -0700

    Add reducers to rootReduver

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import { paletteReducer } from './paletteReducer';
+import { projectReducer } from './projectReducer'; 
 
 export const rootReducer = combineReducers({
-  
+  palettes: paletteReducer,
+  projects: projectReducer
 });
+
+export default rootReducer;
