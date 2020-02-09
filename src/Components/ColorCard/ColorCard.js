ca98f165c08219e81121d4879dce4e07c35cedda
Kayla Wood <kaylaewood@gmail.com>
2020-02-09 16:02:38 -0700

    Change button display based on lock status

--- src/Components/ColorCard/ColorCard.js
+++ src/Components/ColorCard/ColorCard.js
@@ -5,11 +5,19 @@ import PropTypes from 'prop-types';
 function ColorCard({ cardColor, lockColor }) {
   const [ locked, switchLock ] = useState();
 
+  const showLockStatus = () => {
+    if (cardColor.locked) {
+      return `unlock`;
+    } else {
+      return `lock`
+    }
+  }
+
   return (
     <div className={`color-card color-card${cardColor.id}`} style={{
       backgroundColor: `#${cardColor.color}`
     }}>
-      <button className='lock-button' onClick={event => lockColor(event, cardColor.id)}>lock</button>
+      <button className='lock-button' onClick={event => lockColor(event, cardColor.id)}>{showLockStatus()}</button>
       <p>#{(`${cardColor.color}`).toUpperCase()}</p>
     </div>
   );
