7b643b8dc371b27780bfd82b7f421fd7961e227a
Ben Firth <bfirth93@gmail.com>
2020-02-08 22:37:45 -0700

    Create add project ability in projectContainer

--- src/Components/ProjectContainer/ProjectContainer.js
+++ src/Components/ProjectContainer/ProjectContainer.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { apiRequest } from '../../utils/api';
+import { setAllProjects } from '../../actions'
 import './ProjectContainer.scss';
 import Project from '../Project/Project';
 import PropTypes from 'prop-types';
 
-function ProjectContainer() {
+const ProjectContainer = () => {
+  const dispatch = useDispatch();
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProjects = async () => {
+    try {
+      const response = await apiRequest('projects', 'GET');
+      setProjects(response);
+      dispatch(setAllProjects(response));
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  };
+
   return (
     <section>
-      <Project />
-      <Project />
+      {projects.map(project => <Project projectInfo={project} />)}
     </section>
   );
-}
+};
 
 export default ProjectContainer;
