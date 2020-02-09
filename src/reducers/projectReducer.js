d49627ed3518b133f81ea0d415d93c516a15d72e
Ben Firth <bfirth93@gmail.com>
2020-02-08 22:35:49 -0700

    Create add all projects reducer

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -4,6 +4,8 @@ export const Project = (state = [], action) => {
       return [...state, action.project];
     case 'DELETE_PROJECT':
       return [...state].filter(project => project.id !== action.id);
+    case 'SET_ALL_PROJECTS':
+      return action.projects;
     default:
       return state;
   }
