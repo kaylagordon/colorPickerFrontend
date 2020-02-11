fb2f8987a487bbc8021db0e295b20143b3a7f72c
Kayla Wood <kaylaewood@gmail.com>
2020-02-11 08:02:34 -0700

    Display palette on DOM (no color)

--- src/Components/Palette/Palette.js
+++ src/Components/Palette/Palette.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Palette.scss';
 import PropTypes from 'prop-types';
 
-function Palette() {
+function Palette({ palette }) {
   return (
-    <section>
-      <p>Palette 1</p>
-      <div className='color1'></div>
-      <div className='color2'></div>
-      <div className='color3'></div>
-      <div className='color4'></div>
-      <div className='color5'></div>
+    <section className='saved-palette-container'>
+      <p>{palette.name}:</p>
+      <div className='color1'>x</div>
+      <div className='color2'>x</div>
+      <div className='color3'>x</div>
+      <div className='color4'>x</div>
+      <div className='color5'>x</div>
       <button className='delete-palette-button'>X</button>
     </section>
   );
--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './Project.scss';
 import Palette from '../Palette/Palette';
 import PropTypes from 'prop-types';
 
 function Project({ projectInfo }) {
-  const { name } = projectInfo;
+  const { name, id } = projectInfo;
+  const palettes = useSelector(state => state.palettes);
+
+  const displayPalettes = () => {
+    const projectPalettes = palettes.filter(palette => parseInt(palette.project_id) === id);
+
+    return projectPalettes.map(palette => <Palette palette={palette}/>)
+  };
+
 
   return (
     <section>
       <h3>{projectInfo.name}</h3>
-      <Palette />
+      {displayPalettes()}
     </section>
   );
 }
